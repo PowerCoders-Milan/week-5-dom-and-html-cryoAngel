522d631747b3db5afb785bdfbe13cfb228db57ea
Grace Kabeya <antoinefaith1@gmail.com>
2021-03-02 14:31:54 +0100

    finished

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,12 @@
 function getAttributes() {
-
+   
+var a = document.getElementById("pwcdr");
+var type = a.getAttribute("type");
+var hreflang = a.getAttribute("hreflang");
+var rel = a.getAttribute("rel");
+var target = a.getAttribute("target");
+document.getElementById("pwcdr").innerHTML = type;
+document.getElementById("pwcdr").innerHTML = hreflang;
+document.getElementById("pwcdr").innerHTML = rel;
+document.getElementById("pwcdr").innerHTML = target;
 }
