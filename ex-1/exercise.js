021b5d6082cb9d5fbd2997bba27784f0f6bb8809
Grace Kabeya <antoinefaith1@gmail.com>
2021-03-02 12:25:00 +0100

    finished

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,5 +1,10 @@
 // Use this function that is linked to the button to change the background
 
 function set_background() {
-
+    
+    document.body.style.backgroundColor = "blue";
+    document.body.style.color = "white";
+    document.getElementById("first-child").style.backgroundColor = "red";
+    document.getElementById("second-child").style.backgroundColor = "pink";
+    document.getElementById("third-child").style.backgroundColor = "yellow";
 }
